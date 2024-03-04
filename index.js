376a881c2337cb3e8884f7cd1fb1ab0cbc957fad
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2024-03-04 09:07:28 +0600

    dynamic route added

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const toy = require('./data/actionfigure.json');
+
 app.get('/', (req, res) => {
   res.send('marveltoy store API is running')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/toy', (req,res) => {
+    res.send(toy);
+})
+
+app.get('/toy/:id', (req,res) => {
+    const id = req.params.id;
+    const selectedToy = toy.find(toyfigure => toyfigure.id === id);
+    res.send(selectedToy);
 })

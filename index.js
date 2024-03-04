8508bef26b5beb94c6c9a825b06c2e5b8dfa8b84
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2024-03-04 11:55:17 +0600

    get toy by id addedd

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('marveltoy store API is running')
+    res.send('marveltoy store API is running')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/toy', (req,res) => {
+app.get('/toy', (req, res) => {
     res.send(toy);
 })
 
-app.get('/toy/:id', (req,res) => {
+app.get('/toy/:id', (req, res) => {
     const id = req.params.id;
     const selectedToy = toy.find(toyfigure => toyfigure.id === id);
     res.send(selectedToy);
 })
 
-app.get('/membership', (req,res) => {
+app.get('/membership', (req, res) => {
     res.send(membership);
 })
 
-app.get('/membership/:id', (req,res) => {
+app.get('/membership/:id', (req, res) => {
     const id = req.params.id;
     const seletedMembership = membership.find(cardtype => cardtype.id === id);
     res.send(seletedMembership);
@@ -46,46 +46,55 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    // Connect to toy-marketplace db and collection called 'toycollection'
-    const userCollection = client.db('toy-marketplace').collection('toycollection');
+        // Connect to toy-marketplace db and collection called 'toycollection'
+        const userCollection = client.db('toy-marketplace').collection('toycollection');
 
-    app.post('/addtoy', async (req,res) => {
-        const result = await userCollection.insertOne(req.body);
-        res.send(result);
-    })
+        app.post('/addtoy', async (req, res) => {
+            const result = await userCollection.insertOne(req.body);
+            res.send(result);
+        })
 
 
-    app.get('/addtoy', async(req,res) => {
-        const cursor = userCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    });
+        app.get('/addtoy', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-    app.get('/addtoy/email/:selleremail', async(req,res) => {
-        const sellerEmail = req.params.selleremail;
-        // const query = {selleremail: sellerEmail };
-        const user = await userCollection.find({ selleremail: sellerEmail }).toArray();
-        res.send(user);
-      })
+        app.get('/addtoy/email/:selleremail', async (req, res) => {
+            const sellerEmail = req.params.selleremail;
+            // const query = {selleremail: sellerEmail };
+            const user = await userCollection.find({ selleremail: sellerEmail }).toArray();
+            res.send(user);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/addtoy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
+
+      
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);

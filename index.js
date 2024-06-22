63d2264f7645152e55fc5e5f23f4a4543fa9394b
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2024-06-22 19:11:52 +0600

    vercel reload issue fixed

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // const database = client.db("usersDB");
     // const userCollection = database.collection("users");
